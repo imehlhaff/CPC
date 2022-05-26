0849ef14a6a70baa308ea2042189148c9f18cb16
Isaac Mehlhaff <isaac.mehlhaff@gmail.com>
2022-05-26 13:49:21 -0400

    Edit package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("\nTo cite in publications and working papers, please use:\n")
-  packageStartupMessage(" Mehlhaff, Isaac D. A Group-Based Approach to Measuring Polarization. Working Paper. May 2022.")
+  packageStartupMessage(" Mehlhaff, Isaac D. A Group-Based Approach to Measuring Polarization. Working Paper. May 2022.\n")
 }

e8f0a3307e6fd567c929434fc61f58ad59fcae88
Isaac Mehlhaff <isaac.mehlhaff@gmail.com>
2022-05-30 13:00:35 -0400

    Remove release question

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,8 @@
   packageStartupMessage("\nTo cite in publications and working papers, please use:\n")
   packageStartupMessage(" Mehlhaff, Isaac D. A Group-Based Approach to Measuring Polarization. Working Paper. May 2022.\n")
 }
+
+release_questions <- function() {
+  c("Have you incremented the version number?",
+    "Have you formatted cran-comments.md appropriately?")
+}
